a9acd6f427397d3a9a722f7017628dc0b6af650e
fitra <fitraabadi12@gmail.com>
2025-05-05 11:57:54 +0800

    percobaan-3 done

--- resources/views/majors/edit.blade.php
+++ resources/views/majors/edit.blade.php
@@ -1,6 +1,6 @@
 <x-default-layout title="Major" section_title="Edit Major Data">
     <div class="grid grid-cols-1">
-        <form action="#" method="POST"
+        <form action="{{ route('majors.update', $major->id) }}" method="POST"
             class="flex flex-col gap-4 px-6 py-4 bg-white border border-zinc-300 shadow col-span-1">
             @csrf
             @method('PUT')
@@ -8,19 +8,22 @@
             <div class="flex flex-col gap-2">
                 <label for="name">Major Name</label>
                 <input type="text" id="name" name="name" class="px-3 py-2 border border-zinc-300 bg-slate-50"
-                    value="Teknik Informatika" placeholder="e.g., Teknik Informatika" />
+                    value="{{ old('name', $major->name) }}" placeholder="e.g., Teknik Informatika" />
+                @error('name')
+                    <div class="text-red-500">{{ $message }}</div>
+                @enderror
             </div>
 
             <div class="flex flex-col gap-2">
                 <label for="code">Major Code</label>
                 <input type="text" id="code" name="code" class="px-3 py-2 border border-zinc-300 bg-slate-50"
-                    value="F551" placeholder="e.g., TIF" />
+                    value="{{ old('code', $major->code) }}" placeholder="e.g., TIF" />
             </div>
 
             <div class="flex flex-col gap-2">
                 <label for="description">Description</label>
                 <textarea id="description" name="description" class="px-3 py-2 border border-zinc-300 bg-slate-50" rows="3"
-                    placeholder="Write a brief description...">Teknik Informatika adalah bidang ilmu yang mempelajari penggunaan teknologi komputer untuk mengolah data. Jurusan ini mempelajari teori, konsep, dan penerapan praktis ilmu komputer.</textarea>
+                    placeholder="Write a brief description...">{{ old('description', $major->description) }}</textarea>
             </div>
 
             <div class="self-end flex gap-2 mt-4">
