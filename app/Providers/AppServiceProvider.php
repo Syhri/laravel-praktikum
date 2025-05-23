6299a20aa865cd59051cb25b23d228a23b276b83
Syahril Fitrawan Abadi <fitraabadi12@gmail.com>
2025-05-23 11:16:10 +0800

    update appservices

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,7 +23,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         if (env('APP_ENV') === 'production') {
-            URL:forceScheme('https');
+            URL::forceScheme('https');
         }
         
         Gate::define("view-student", function (User $user) {
